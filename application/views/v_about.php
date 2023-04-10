898c2fb494e95c102377727879130565418bb6fc
kzpnisa1304 <khairunnisazp13@gmail.com>
2023-04-10 16:54:16 +0700

    Pertemuan 4 dan 5

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Khairunnisa Zakaria</li>
+ <li>Nama Belakang : Putri</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan HR. Rasuna Said Kp. Sawah Dalam No 83 Tangerang </li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li> Jakarta </li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li> Bulutangkis </li>
+ <li> Renang </li>
+ </ul>
+</section>
--- system/language/english/form_validation_lang.php
+++ system/language/english/form_validation_lang.php
@@ -38,14 +38,14 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['form_validation_required']		= 'The {field} field is required.';
+$lang['form_validation_required']		= 'field {field} ini harus di isi.';
 $lang['form_validation_isset']			= 'The {field} field must have a value.';
 $lang['form_validation_valid_email']		= 'The {field} field must contain a valid email address.';
 $lang['form_validation_valid_emails']		= 'The {field} field must contain all valid email addresses.';
 $lang['form_validation_valid_url']		= 'The {field} field must contain a valid URL.';
 $lang['form_validation_valid_ip']		= 'The {field} field must contain a valid IP.';
 $lang['form_validation_valid_base64']		= 'The {field} field must contain a valid Base64 string.';
-$lang['form_validation_min_length']		= 'The {field} field must be at least {param} characters in length.';
+$lang['form_validation_min_length']		= '{field} kurang dari {param} karakter.';
 $lang['form_validation_max_length']		= 'The {field} field cannot exceed {param} characters in length.';
 $lang['form_validation_exact_length']		= 'The {field} field must be exactly {param} characters in length.';
 $lang['form_validation_alpha']			= 'The {field} field may only contain alphabetical characters.';
